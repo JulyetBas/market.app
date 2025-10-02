ce75d2349fa5bb700d5246986b9f1a1868eb6ae1
Julieth Bas <sparks.and.honey24@gmail.com>
2025-10-02 11:19:06 -0500

    validate login with MD5 and go to main page

---
+++ src/main.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome to main!!!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketapp- home </title>
+</head>
+<body>
+    
+</body>
+</html>
--- src/signin.php
+++ src/signin.php
@@ -3,9 +3,11 @@
 // Stemp 1. Get database connection
 require ('../config/database.php');
 // Stemp 2. Get form-data
-$e_mail =$_POST['email'];
-$p_wd =$_POST['passwd'];
+$e_mail =trim($_POST['email']);
+$p_wd =trim($_POST['passwd']);
 //Stemp 3. Query to validate data
+//$enc_pass = password_hash($p_wd,PASSWORD_DEFAULT);
+$end_pass=md5($p_wd);
 $sql_check_user ="
 select 
 	u.email, u.password
@@ -13,13 +15,14 @@ select
 	users u
 	where 
 	u.email ='$e_mail' and 
-	u.password ='$p_wd'
+	u.password ='$end_pass'
 	limit 1
 ";
 //Stemp 4.  Execute query
 $res_check= pg_query($conn, $sql_check_user);
 if(pg_num_rows($res_check)>0){
-    echo"User exists. Go to main page!!!";
+    //echo"User exists. Go to main page!!!";
+header('refresh:0;url=main.php');
 }else{
     echo"Verify data";
 }
